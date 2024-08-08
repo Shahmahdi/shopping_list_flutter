2007708b018339eae182e8f4d63a20d4b449f379
Shahmahdi <shahmahdi11@gmail.com>
2024-08-09 02:41:56 +0600

    pilot commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: "Shopping list app",
+      home: Text("Shopping list app"),
+    ),
+  );
+}
